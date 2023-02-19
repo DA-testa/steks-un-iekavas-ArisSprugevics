45be985a1c0410c9c750c67cadcd57710550ad1a
ArisSprugevics <aris.sprugevics@edu.rtu.lv>
2023-02-19 22:33:37 +0200

    ver 1.0

--- main.py
+++ main.py
@@ -13,17 +13,29 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
+            opening_brackets_stack.append(next) #Bracket(next,i+1)
             # Process opening bracket, write your code here
             pass
 
         if next in ")]}":
+            if not bool(opening_brackets_stack) or not are_matching(opening_brackets_stack.pop(),next):
+                return(i)
             # Process closing bracket, write your code here
+            # opening_brackets_stack.pop()
             pass
 
 
+
+
 def main():
+    print("input text: ")
     text = input()
     mismatch = find_mismatch(text)
+
+    if mismatch is None:
+        print("Success")
+    else:
+        print(mismatch)
     # Printing answer, write your code here
 
 
